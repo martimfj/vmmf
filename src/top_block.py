38d02721fbd1a4def695cd2b13b929a811298119
martimfj <martim.ferreira.jose@gmail.com>
2017-10-25 19:44:41 -0200

    Blocos de transmissao alterados

--- src/top_block.py
+++ src/top_block.py
@@ -3,7 +3,7 @@
 ##################################################
 # GNU Radio Python Flow Graph
 # Title: Top Block
-# Generated: Mon Oct 23 17:14:12 2017
+# Generated: Wed Oct 25 18:40:44 2017
 ##################################################
 
 if __name__ == '__main__':
@@ -22,7 +22,6 @@ from gnuradio import audio
 from gnuradio import blocks
 from gnuradio import digital
 from gnuradio import eng_notation
-from gnuradio import filter
 from gnuradio import gr
 from gnuradio import qtgui
 from gnuradio.eng_option import eng_option
@@ -62,65 +61,26 @@ class top_block(gr.top_block, Qt.QWidget):
         ##################################################
         # Variables
         ##################################################
-        self.sps = sps = 45
+        self.sps_slider = sps_slider = 45
         self.nfilts = nfilts = 25
+        self.volume_slider = volume_slider = 1
         self.samp_rate = samp_rate = 44.1E3
-        self.rrc_taps = rrc_taps = firdes.root_raised_cosine(nfilts, nfilts, 1.0/float(sps), 0.35, 45*nfilts)
+        self.rrc_taps = rrc_taps = firdes.root_raised_cosine(nfilts, nfilts, 1.0/float(sps_slider), 0.35, 45*nfilts)
         self.fc_slider = fc_slider = 2200
         self.BPSK = BPSK = digital.constellation_calcdist(([-1, 1]), ([0, 1]), 4, 1).base()
 
         ##################################################
         # Blocks
         ##################################################
+        self._volume_slider_range = Range(-1, 2, 0.1, 1, 1)
+        self._volume_slider_win = RangeWidget(self._volume_slider_range, self.set_volume_slider, "volume", "counter_slider", float)
+        self.top_layout.addWidget(self._volume_slider_win)
+        self._sps_slider_range = Range(10, 80, 1, 45, 10)
+        self._sps_slider_win = RangeWidget(self._sps_slider_range, self.set_sps_slider, "sps", "counter_slider", float)
+        self.top_layout.addWidget(self._sps_slider_win)
         self._fc_slider_range = Range(0, 18200, 200, 2200, 150)
         self._fc_slider_win = RangeWidget(self._fc_slider_range, self.set_fc_slider, "fc", "counter_slider", float)
         self.top_layout.addWidget(self._fc_slider_win)
-        self.qtgui_time_sink_x_0_0 = qtgui.time_sink_f(
-        	1024, #size
-        	samp_rate, #samp_rate
-        	"", #name
-        	1 #number of inputs
-        )
-        self.qtgui_time_sink_x_0_0.set_update_time(0.10)
-        self.qtgui_time_sink_x_0_0.set_y_axis(-1, 1)
-        
-        self.qtgui_time_sink_x_0_0.set_y_label("Amplitude", "")
-        
-        self.qtgui_time_sink_x_0_0.enable_tags(-1, True)
-        self.qtgui_time_sink_x_0_0.set_trigger_mode(qtgui.TRIG_MODE_FREE, qtgui.TRIG_SLOPE_POS, 0.0, 0, 0, "")
-        self.qtgui_time_sink_x_0_0.enable_autoscale(False)
-        self.qtgui_time_sink_x_0_0.enable_grid(False)
-        self.qtgui_time_sink_x_0_0.enable_control_panel(False)
-        
-        if not True:
-          self.qtgui_time_sink_x_0_0.disable_legend()
-        
-        labels = ["", "", "", "", "",
-                  "", "", "", "", ""]
-        widths = [1, 1, 1, 1, 1,
-                  1, 1, 1, 1, 1]
-        colors = ["blue", "red", "green", "black", "cyan",
-                  "magenta", "yellow", "dark red", "dark green", "blue"]
-        styles = [1, 1, 1, 1, 1,
-                  1, 1, 1, 1, 1]
-        markers = [-1, -1, -1, -1, -1,
-                   -1, -1, -1, -1, -1]
-        alphas = [1.0, 1.0, 1.0, 1.0, 1.0,
-                  1.0, 1.0, 1.0, 1.0, 1.0]
-        
-        for i in xrange(1):
-            if len(labels[i]) == 0:
-                self.qtgui_time_sink_x_0_0.set_line_label(i, "Data {0}".format(i))
-            else:
-                self.qtgui_time_sink_x_0_0.set_line_label(i, labels[i])
-            self.qtgui_time_sink_x_0_0.set_line_width(i, widths[i])
-            self.qtgui_time_sink_x_0_0.set_line_color(i, colors[i])
-            self.qtgui_time_sink_x_0_0.set_line_style(i, styles[i])
-            self.qtgui_time_sink_x_0_0.set_line_marker(i, markers[i])
-            self.qtgui_time_sink_x_0_0.set_line_alpha(i, alphas[i])
-        
-        self._qtgui_time_sink_x_0_0_win = sip.wrapinstance(self.qtgui_time_sink_x_0_0.pyqwidget(), Qt.QWidget)
-        self.top_layout.addWidget(self._qtgui_time_sink_x_0_0_win)
         self.qtgui_time_sink_x_0 = qtgui.time_sink_f(
         	1024, #size
         	samp_rate, #samp_rate
@@ -207,34 +167,18 @@ class top_block(gr.top_block, Qt.QWidget):
         
         self._qtgui_const_sink_x_0_win = sip.wrapinstance(self.qtgui_const_sink_x_0.pyqwidget(), Qt.QWidget)
         self.top_layout.addWidget(self._qtgui_const_sink_x_0_win)
-        self.low_pass_filter_0 = filter.fir_filter_ccf(1, firdes.low_pass(
-        	1, samp_rate, 1.1E3, .6E3, firdes.WIN_HAMMING, 6.76))
-        self.digital_pfb_clock_sync_xxx_0 = digital.pfb_clock_sync_ccf(sps, .063, (rrc_taps), nfilts, nfilts/2, 1.5, 1)
-        self.digital_lms_dd_equalizer_cc_0 = digital.lms_dd_equalizer_cc(8, .01, 1, BPSK)
-        self.digital_diff_decoder_bb_0 = digital.diff_decoder_bb(2)
-        self.digital_costas_loop_cc_0 = digital.costas_loop_cc(.05, 2, False)
         self.digital_constellation_modulator_0 = digital.generic_mod(
           constellation=BPSK,
           differential=True,
-          samples_per_symbol=sps,
+          samples_per_symbol=sps_slider,
           pre_diff_code=True,
           excess_bw=0.25,
           verbose=False,
           log=False,
           )
-        self.digital_binary_slicer_fb_0 = digital.binary_slicer_fb()
-        self.blocks_multiply_xx_0_0 = blocks.multiply_vcc(1)
         self.blocks_multiply_xx_0 = blocks.multiply_vcc(1)
-        self.blocks_float_to_complex_0 = blocks.float_to_complex(1)
-        self.blocks_file_source_0 = blocks.file_source(gr.sizeof_char*1, "/home/borg/vmmf/Exemplo/Drummond-residos.txt", True)
+        self.blocks_file_source_0 = blocks.file_source(gr.sizeof_char*1, "/home/martimfj/Dropbox/Insper/4/CamadaFísica/Projetos/vmmf/teste.txt", True)
         self.blocks_complex_to_real_1 = blocks.complex_to_real(1)
-        self.blocks_complex_to_real_0 = blocks.complex_to_real(1)
-        self.blks2_tcp_sink_0 = grc_blks2.tcp_sink(
-        	itemsize=gr.sizeof_char*1,
-        	addr="127.0.0.1",
-        	port=1234,
-        	server=False,
-        )
         self.blks2_packet_encoder_0 = grc_blks2.packet_mod_b(grc_blks2.packet_encoder(
         		samples_per_symbol=1,
         		bits_per_symbol=1,
@@ -244,43 +188,20 @@ class top_block(gr.top_block, Qt.QWidget):
         	),
         	payload_length=1,
         )
-        self.blks2_packet_decoder_0 = grc_blks2.packet_demod_b(grc_blks2.packet_decoder(
-        		access_code="",
-        		threshold=-1,
-        		callback=lambda ok, payload: self.blks2_packet_decoder_0.recv_pkt(ok, payload),
-        	),
-        )
-        self.audio_source_0 = audio.source(44100, "", True)
         self.audio_sink_0 = audio.sink(44100, "", True)
-        self.analog_sig_source_x_0_0 = analog.sig_source_c(samp_rate, analog.GR_COS_WAVE, fc_slider, 1, 0)
-        self.analog_sig_source_x_0 = analog.sig_source_c(samp_rate, analog.GR_COS_WAVE, fc_slider, 1, 0)
-        self.analog_feedforward_agc_cc_0 = analog.feedforward_agc_cc(1024, 1)
+        self.analog_sig_source_x_0 = analog.sig_source_c(samp_rate, analog.GR_COS_WAVE, fc_slider, volume_slider, 0)
 
         ##################################################
         # Connections
         ##################################################
-        self.connect((self.analog_feedforward_agc_cc_0, 0), (self.digital_pfb_clock_sync_xxx_0, 0))    
         self.connect((self.analog_sig_source_x_0, 0), (self.blocks_multiply_xx_0, 1))    
-        self.connect((self.analog_sig_source_x_0_0, 0), (self.blocks_multiply_xx_0_0, 1))    
-        self.connect((self.audio_source_0, 0), (self.blocks_float_to_complex_0, 0))    
-        self.connect((self.audio_source_0, 0), (self.qtgui_time_sink_x_0_0, 0))    
-        self.connect((self.blks2_packet_decoder_0, 0), (self.blks2_tcp_sink_0, 0))    
         self.connect((self.blks2_packet_encoder_0, 0), (self.digital_constellation_modulator_0, 0))    
-        self.connect((self.blocks_complex_to_real_0, 0), (self.digital_binary_slicer_fb_0, 0))    
         self.connect((self.blocks_complex_to_real_1, 0), (self.audio_sink_0, 0))    
         self.connect((self.blocks_complex_to_real_1, 0), (self.qtgui_time_sink_x_0, 0))    
         self.connect((self.blocks_file_source_0, 0), (self.blks2_packet_encoder_0, 0))    
-        self.connect((self.blocks_float_to_complex_0, 0), (self.blocks_multiply_xx_0_0, 0))    
         self.connect((self.blocks_multiply_xx_0, 0), (self.blocks_complex_to_real_1, 0))    
-        self.connect((self.blocks_multiply_xx_0_0, 0), (self.low_pass_filter_0, 0))    
-        self.connect((self.digital_binary_slicer_fb_0, 0), (self.digital_diff_decoder_bb_0, 0))    
+        self.connect((self.blocks_multiply_xx_0, 0), (self.qtgui_const_sink_x_0, 0))    
         self.connect((self.digital_constellation_modulator_0, 0), (self.blocks_multiply_xx_0, 0))    
-        self.connect((self.digital_costas_loop_cc_0, 0), (self.digital_lms_dd_equalizer_cc_0, 0))    
-        self.connect((self.digital_diff_decoder_bb_0, 0), (self.blks2_packet_decoder_0, 0))    
-        self.connect((self.digital_lms_dd_equalizer_cc_0, 0), (self.blocks_complex_to_real_0, 0))    
-        self.connect((self.digital_lms_dd_equalizer_cc_0, 0), (self.qtgui_const_sink_x_0, 0))    
-        self.connect((self.digital_pfb_clock_sync_xxx_0, 0), (self.digital_costas_loop_cc_0, 0))    
-        self.connect((self.low_pass_filter_0, 0), (self.analog_feedforward_agc_cc_0, 0))    
 
     def closeEvent(self, event):
         self.settings = Qt.QSettings("GNU Radio", "top_block")
@@ -288,19 +209,26 @@ class top_block(gr.top_block, Qt.QWidget):
         event.accept()
 
 
-    def get_sps(self):
-        return self.sps
+    def get_sps_slider(self):
+        return self.sps_slider
 
-    def set_sps(self, sps):
-        self.sps = sps
-        self.set_rrc_taps(firdes.root_raised_cosine(self.nfilts, self.nfilts, 1.0/float(self.sps), 0.35, 45*self.nfilts))
+    def set_sps_slider(self, sps_slider):
+        self.sps_slider = sps_slider
+        self.set_rrc_taps(firdes.root_raised_cosine(self.nfilts, self.nfilts, 1.0/float(self.sps_slider), 0.35, 45*self.nfilts))
 
     def get_nfilts(self):
         return self.nfilts
 
     def set_nfilts(self, nfilts):
         self.nfilts = nfilts
-        self.set_rrc_taps(firdes.root_raised_cosine(self.nfilts, self.nfilts, 1.0/float(self.sps), 0.35, 45*self.nfilts))
+        self.set_rrc_taps(firdes.root_raised_cosine(self.nfilts, self.nfilts, 1.0/float(self.sps_slider), 0.35, 45*self.nfilts))
+
+    def get_volume_slider(self):
+        return self.volume_slider
+
+    def set_volume_slider(self, volume_slider):
+        self.volume_slider = volume_slider
+        self.analog_sig_source_x_0.set_amplitude(self.volume_slider)
 
     def get_samp_rate(self):
         return self.samp_rate
@@ -308,17 +236,13 @@ class top_block(gr.top_block, Qt.QWidget):
     def set_samp_rate(self, samp_rate):
         self.samp_rate = samp_rate
         self.analog_sig_source_x_0.set_sampling_freq(self.samp_rate)
-        self.analog_sig_source_x_0_0.set_sampling_freq(self.samp_rate)
-        self.low_pass_filter_0.set_taps(firdes.low_pass(1, self.samp_rate, 1.1E3, .6E3, firdes.WIN_HAMMING, 6.76))
         self.qtgui_time_sink_x_0.set_samp_rate(self.samp_rate)
-        self.qtgui_time_sink_x_0_0.set_samp_rate(self.samp_rate)
 
     def get_rrc_taps(self):
         return self.rrc_taps
 
     def set_rrc_taps(self, rrc_taps):
         self.rrc_taps = rrc_taps
-        self.digital_pfb_clock_sync_xxx_0.update_taps((self.rrc_taps))
 
     def get_fc_slider(self):
         return self.fc_slider
@@ -326,7 +250,6 @@ class top_block(gr.top_block, Qt.QWidget):
     def set_fc_slider(self, fc_slider):
         self.fc_slider = fc_slider
         self.analog_sig_source_x_0.set_frequency(self.fc_slider)
-        self.analog_sig_source_x_0_0.set_frequency(self.fc_slider)
 
     def get_BPSK(self):
         return self.BPSK
